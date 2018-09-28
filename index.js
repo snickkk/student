6ca1a648aa68c3d8d393debb06140da081d7b74b
Anton <snickanton@gmail.com>
2018-09-28 22:31:11 +0300

    seconcode12

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ var circle = document.querySelector ( ".circle" )
 var button = document.querySelector ( "button" )
 
 button.onclick = function ( event ) {
-//        circle.style.visibility = circle.style.visibility === "visible" ?
-//                                     "hidden" : "visible"
-  var ring = document.body.appendChild (
-    document.createElement ('div')
-  )
-  var radius = Math.random () * 300
-  var colours = ['red','yellow','pink','blue','black']
-  var colour = colours [Math.floor (Math.random () * colours.lenght)]
-
-  var x = Math.random() * window.innerWidth
-  var y = Math.random() * window.innerHeight
-  ring.style = `
-  width : ${radius}px ;
-  height : ${radius}px ;
-  border-radius : 50% ;
-  border : solid 1px ${colour} ;
-  position : fixed ;
-  top : ${y}px ;
-  left : ${x}px ;
-
-`
+        //circle.style.visibility = circle.style.visibility === "visible" ?
+                                    // "hidden" : "visible"
+          var ring = document.body.appendChild (
+              document.createElement ("div")
+          )
+          var radius = Math.random () * 300
+          var colours = ["red","yellow","white","pink","blue","black"]
+          var colour = colours[Math.floor ( Math.random () * colours.length )]
+          var x = Math.random () * window.innerWidth
+          var y = Math.random () * window.innerHeight
+          ring.style = `
+              width:${radius}px;
+              height:${radius}px;
+              border-radius:50%;
+              border:solid 1px ${colour};
+              position:fixed;
+              top:${y}px;
+              left:${x}px;
+          `
 }
